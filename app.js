c660aade71b9b4ef242b03b11c1bb4f63b913c7a
amitsk <amitsk2014@gmail.com>
2022-11-19 17:57:12 -0500

    cors fixes part 2

--- app.js
+++ app.js
@@ -5,14 +5,22 @@ import TuitsController from "./controllers/tuits/tuits-controller.js";
 import cors from 'cors'
 const app = express();
 app.use(express.json())
-app.use(cors(
-    {
-        origin:["https://a8--nimble-salamander-6b50f8.netlify.app"],
-        methods: ["GET","POST","PUT","DELETE"],
-        credentials: true
-    }
-));
+app.use((req,res,next)=>{
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+})
+// app.use(cors(
+//     {
+//         origin:["https://a8--nimble-salamander-6b50f8.netlify.app"],
+//         methods: ["GET","POST","PUT","DELETE"],
+//         credentials: true
+//     }
+// ));
 
+app.use(cors());
 HelloController(app)
 UserController(app);
 TuitsController(app);
