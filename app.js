7ae25c510b09c241a9a0b67eeac7fb33c67f81c2
amitsk <amitsk2014@gmail.com>
2022-11-19 21:03:20 -0500

    changed the sequence

--- app.js
+++ app.js
@@ -1,29 +1,12 @@
 import express  from 'express';
+import cors from 'cors'
 import HelloController from './controllers/hello-controller.js'
 import UserController from "./controllers/users/users-controller.js";
 import TuitsController from "./controllers/tuits/tuits-controller.js";
-import cors from 'cors'
 const app = express();
-app.use(express.json())
-app.use((req,res,next)=>{
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    next();
-})
-// app.use(cors(
-//     {
-//         origin:["https://a8--nimble-salamander-6b50f8.netlify.app"],
-//         methods: ["GET","POST","PUT","DELETE"],
-//         credentials: true
-//     }
-// ));
-
 app.use(cors());
+app.use(express.json());
+TuitsController(app);
 HelloController(app)
 UserController(app);
-TuitsController(app);
-
-
 app.listen(process.env.PORT || 4000)
