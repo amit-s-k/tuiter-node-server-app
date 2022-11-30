a6b93f9ae580282dc787078b324ce87c7840ea1d
amitsk <amitsk2014@gmail.com>
2022-11-29 20:25:21 -0500

    added mongo connection string

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(express.json());
 const MONGO_ATLAS = 'mongodb+srv://mymongodb:keepitsimple@cluster0.b0yoet4.mongodb.net/tuiter?retryWrites=true&w=majority';
 let CONNECTION_STRING = process.env.DB_CONNECTION_STRING === undefined? MONGO_ATLAS : process.env.DB_CONNECTION_STRING;
 const MONGO_LOCAL = 'mongodb://localhost:27017/tuiter';
-mongoose.connect(MONGO_LOCAL);
+mongoose.connect(CONNECTION_STRING);
 TuitsController(app);
 HelloController(app)
 UserController(app);
